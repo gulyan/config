cde7198cab3d70ffac7cc6f5af882efb370d881a
Alexandru Guduleasa <alexandru.guduleasa@gmail.com>
2013-05-07 13:48:00 +0300

    Don't check certificate of github
    Cygwin's wget will fail because of the https

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ URL=raw.github.com/gulyan/config/master
 function install_file {
 	FILE=$1
 	echo "Gettind $FILE from $URL"
-	wget -O - $URL/$FILE 2> /dev/null >~/.$FILE || exit 1
-	echo "Copied to ~/.$FILE"
+	wget --no-check-certificate -O - $URL/$FILE 2> /dev/null >~/.$FILE && \
+		echo "Copied to ~/.$FILE"
 }
 
 install_file "vimrc"
